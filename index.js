2121eeca2e206f9387efc36e437c5f177cce477e
BuildTools <unconfigured@null.spigotmc.org>
2021-12-11 01:33:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on('connected', (adress, port) => {
 
 client.on('chat', (channel, user, message, self,) => {
     if(message === '!socials' || message === '!Socials' ) {
-        client.action('freitags_gaming', 'Folge Benston auch auf: https://twitter.com/Benston_ • https://youtube.com/Benston • https://instagram.com/benstontv • https://www.tiktok.com/@benston_ • https://discord.gg/x9Jn9CA benstonHYPE ❖')
+        client.action('freitags_gaming', 'Folge Benston auch auf:  https://www.youtube.com/channel/UC7wIvyHW7CrY26tD8BI2j5g  https://www.instagram.com/freitags_gaming/  https://www.tiktok.com/@freitagsgaming_clips?  https://discord.gg/k7UYXCU3nc ❖')
     }
 })
 
